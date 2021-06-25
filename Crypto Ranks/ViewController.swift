11a4a90a591fb50ff8a5b28ab0e8fc74d1b8c72b
Xavier Jones <xavjones8@gmail.com>
2021-06-25 18:20:55 -0400

    Initializes Xcode Project

---
+++ Crypto Ranks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto Ranks
+//
+//  Created by Xavier Jones on 6/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
